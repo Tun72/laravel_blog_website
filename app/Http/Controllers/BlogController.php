c096577f3171e9e1e3a9a7c5199f4af74ec7d154
Ttm <tunt72553@gmail.com>
2023-10-15 01:03:02 +0630

    first commit

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -19,7 +19,12 @@ class BlogController extends Controller
     public function show(Blog $blog) {
         return view('blogs.show', [
             'blog' => $blog,
-            'randomBlogs' => Blog::with(["user", "category" ])->inRandomOrder()->take(3)->get()
+            'randomBlogs' => cache()->remember('blogs' . $blog->slug, now()->addSeconds(10), function() use($blog) {
+
+                return Blog::inRandomOrder()->whereHas('category', function($query) use($blog){
+                  $query->where('slug', $blog->category->slug);
+                })->take(3)->get();
+            })
         ]);
     }
 }
