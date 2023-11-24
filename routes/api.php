94cdb2646c16c69e03eea5c2dcfa78e9be5ae01a
Tun72 <tunt72553@gmail.com>
2023-11-24 16:34:39 +0630

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,47 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("/users", function () {
+    try {
+        if (request("is_admin") === "true") {
+            return User::all();
+        } else {
+            return [
+                "message" => "Access denied!",
+                "status" => 200
+            ];
+        }
+    } catch(Exception $e) {
+        return [
+            "message" => $e->getMessage(),
+            "status" => 500
+        ];
+    }
+
+});
+
+
+Route::post("/users", function () {
+    try {
+        if (request("is_admin") === "true") {
+            $cleanData = request()->validate([
+                "name" => ["required"]
+            ]);
+
+            
+        } else {
+            return [
+                "message" => "Access denied!",
+                "status" => 200
+            ];
+        }
+    } catch(Exception $e) {
+        return [
+            "message" => $e->getMessage(),
+            "status" => 500
+        ];
+    }
+
 });
